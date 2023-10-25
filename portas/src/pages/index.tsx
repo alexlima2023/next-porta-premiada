d5e371a6a6ad8a8968d940cb4f5cb49118ea33c4
alexlima2023 <alexlimadev1997@gmail.com>
2023-10-25 16:52:28 -0300

    estruturando formulario de controle de jogo

--- portas/src/pages/index.tsx
+++ portas/src/pages/index.tsx
@@ -1,9 +1,24 @@
+import Cartao from "../components/Cartao";
+import styles from "../styles/Formulario.module.css"
+import Link from "next/link";
 
-export default function Home() {  
-
+export default function Formulario() {  
   return (
-    <div>
-      <h1>Iniciar Jogo</h1>
+    <div className={styles.formulario}>
+      <div>
+        <Cartao bgcolor="#c0392c">
+          <h1>Monty Hall</h1>
+        </Cartao>
+        <Cartao></Cartao>
+      </div>
+      <div>
+        <Cartao></Cartao>
+        <Cartao bgcolor="#28a085">
+          <Link href={`/jogo/4/2`} className={styles.link}>
+            <h2>Iniciar</h2>
+          </Link>
+        </Cartao>
+      </div>
     </div>
   )
 }

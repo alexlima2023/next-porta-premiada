59165dd46d0852eee2084a3b2b2eebe62d29e17e
alexlima2023 <alexlimadev1997@gmail.com>
2023-10-25 15:50:25 -0300

    criando navegação dinamica pelo query da porta com useRouter

--- portas/src/pages/index.tsx
+++ portas/src/pages/index.tsx
@@ -1,22 +1,9 @@
 
-import { useState } from "react"
-import Porta from "../components/Porta"
-import { criarPortas, atualizarPortas } from "../functions/portas"
-
 export default function Home() {  
-  const [portas, setPortas] = useState(criarPortas(4, 3))
-
-  function renderizarPortas() {
-    return portas.map(porta => {
-      return <Porta key={porta.numero} value={porta} onChange={novaPorta => {
-        setPortas(atualizarPortas(portas, novaPorta))
-      }}/>
-    })
-  }
 
   return (
-    <div style={{display: 'flex'}}>
-      { renderizarPortas() }
+    <div>
+      <h1>Iniciar Jogo</h1>
     </div>
   )
 }

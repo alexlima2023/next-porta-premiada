c2bff779ac505462154bd2d9881aa794ad96f0d2
alexlima2023 <alexlimadev1997@gmail.com>
2023-10-25 09:27:55 -0300

    implementando presente a portas

--- portas/src/pages/index.tsx
+++ portas/src/pages/index.tsx
@@ -1,17 +1,22 @@
 
 import { useState } from "react"
 import Porta from "../components/Porta"
-import PortaModel from "../model/porta"
-import criarPortas from "../functions/portas"
+import { criarPortas, atualizarPortas } from "../functions/portas"
 
-export default function Home() {
-  const [p1, setP1] = useState(new PortaModel(1))
+export default function Home() {  
+  const [portas, setPortas] = useState(criarPortas(4, 3))
 
-  console.log(criarPortas(3, 2))
+  function renderizarPortas() {
+    return portas.map(porta => {
+      return <Porta key={porta.numero} value={porta} onChange={novaPorta => {
+        setPortas(atualizarPortas(portas, novaPorta))
+      }}/>
+    })
+  }
 
   return (
     <div style={{display: 'flex'}}>
-      <Porta value={p1} onChange={novaPorta => setP1(novaPorta)}/>
+      { renderizarPortas() }
     </div>
   )
 }
